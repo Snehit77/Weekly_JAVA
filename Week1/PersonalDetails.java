c714072df9038d595b23deb7b48e1f885a21fd29
Snehit Parajuli <psnehit23@tbc.edu.np>
2024-03-25 22:02:55 +0545

    correction

--- Week1/PersonalDetails.java
+++ Week1/PersonalDetails.java
@@ -2,27 +2,18 @@
 that outputs personal details to the screen, e.g. Name, Address, Age, 
 Phone number etc.*/
 
- import java.util.Scanner;
-
  public class PersonalDetails {
      public static void main(String[] args) {
-         Scanner scanner = new Scanner(System.in);
-         System.out.print("Enter your name: ");
-         String name = scanner.nextLine();
+        String name="Snehit Parajuli";
          System.out.println("Your Name is " + name);
 
-         System.out.print("Enter your address: ");
-         String address = scanner.nextLine();
-          System.out.println("Yout Address is " + address);
+         String address="Pepsicola";
+        System.out.println("Yout Address is " + address);
 
-         System.out.print("Enter your age: ");
-         int age = scanner.nextInt();
-         scanner.nextLine();
+        int age=19;
         System.out.println("Your age is " + age);
 
-         System.out.print("Enter your phone number: ");
-         String phoneNumber = scanner.nextLine();
-         System.out.println("Ypur phone number is" + phoneNumber);
-         scanner.close();
+        String phoneNumber="9847820906";
+         System.out.println("Ypur phone number is " + phoneNumber);
      }
  }
